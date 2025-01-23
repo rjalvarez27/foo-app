a8c65efd160305c6654acdd2f940a41b2231d1fe
Raúl Alvarez <raul509@gmail.com>
2025-01-23 13:56:00 -0400

    fead: new appe.py

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route("/")
+
+def hello_world():
+
+    return "<p>Hello, World!</p>"
+
+@app.route("/things")
+
+def things():
+
+    return jsonify({
+
+        "payload":["foo","bar","baz","echo"]
+
+    })
+
+ 
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
